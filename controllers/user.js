b1727409891cc32ab578f743ad437d3544e5ecc6
jirilcs10 <jirilcherianshaju@gmail.com>
2023-12-12 18:06:19 +0530

    passwordencrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,17 +1,17 @@
 const User = require('../models/users');
-
+const bcrypt=require('bcrypt');
 exports.signUp = async (req,res,next)=>{
   try{
   const name = req.body.name;
   const email= req.body.email;
   const password= req.body.password;
-
+  const hash=await bcrypt.hash(password,10)
   const data=await User.create({
     name:name,
     email:email,
-    password:password
+    password:hash
   });
-  res.status(201).json({newUser:data})
+  res.status(201).json({message:"New user created successfully"});
 }
 catch(err)
 {
@@ -31,20 +31,20 @@ catch(err)
   console.log(password);
   if(!email.length>0||!password.length>0)
   {
-    throw new Error("Please enter the details");
+    throw new Error("Please enter the email and password");
   }
 
   data=await User.findOne({ where: { email:email } });
   if(data)
   {
-      if(data.password===password)
-      {
+      bcrypt.compare(password,data.password,(error,result)=>{
+        if(error)
+        throw new Error("Something went wrong");
+        if(result===true)
         return res.status(201).json("User Logged in Successfully");
-      }
-      else
-      {
-        return res.status(401).json("User Not Authorized");
-      }
+        else
+        return res.status(401).json("Password does not match");
+      });
   }
   else
   {
